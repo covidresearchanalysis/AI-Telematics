3074a588a372111f5e1ce9ac44cc3d51924ef462
Sher Chattha <sher@192.168.1.126>
2025-07-16 15:35:57 +0100

    First commit

---
+++ AI telematics/AI_telematicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_telematicsApp.swift
+//  AI telematics
+//
+//  Created by Sher Chattha on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AI_telematicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# AI-Telematics
